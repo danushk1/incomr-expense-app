9786af1545247b6944c016be39aa9b1b702ec44e
Danushkasupun <danushksupun1@gmail.com>
2024-12-14 23:37:49 +0530

    Separated sidebar code into the new component.

--- resources/frontend/src/Components/common/sidebara/SideBar.jsx
+++ resources/frontend/src/Components/common/sidebara/SideBar.jsx
@@ -12,7 +12,7 @@ const SideBar = () => {
                     <path clip-rule="evenodd" fill-rule="evenodd"
                           d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
-            </button>
+            </button>//
             <aside id="logo-sidebar"
                    className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
                    aria-label="Sidebar">
